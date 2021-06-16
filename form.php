bd70700c08df787d8c951c37fbcbb930ed535f8e
Melissa Christiana <machristiana@ufl.edu>
2021-06-16 11:38:12 -0400

    Add and style forms for contact form

---
+++ form.php
@@ -0,0 +1,23 @@
+<!-- HTML top code goes here -->
+
+<?
+
+// From processing takes place here. message displayed here too
+require('form/form.class.php');
+$form = new Form();
+$form->processForm(
+
+	'melissachristiana.com' // Put you mail domain here
+	,
+	'3 Daughters Brewing' // Put your site name / form name here
+	,
+	'machristiana1310@gmail.com, sarah.falter@ufl.edu' // Where will the form notification be sent?
+	,
+	'noreply@melissachristiana.com' // This what the form FROM: address wil be, if the form submissions doesn't contain an email field
+
+);
+
+
+?>
+
+<!-- HTML bottom code goes here -->
---
+++ form/default.inc.php
@@ -0,0 +1,264 @@
+ <!-- Adobe Fonts -->
+ <link rel="stylesheet" href="https://use.typekit.net/mwg6wqu.css">
+
+  <!-- Font Awesome -->
+  <script src="https://kit.fontawesome.com/70a4005e9d.js" crossorigin="anonymous"></script>
+
+<!-- Favicon -->
+<link rel="icon" type="image/png" href="images/favicon.png" />
+<link rel="icon" type="image/ico" href="images/favicon.ico" />
+
+<style type="text/css">
+
+* {
+  border: 0;
+  padding: 0;
+  margin: 0;
+}
+
+h1,
+h2,
+h3 {
+  font-family: 'acumin-pro-condensed', sans-serif;
+}
+p {
+  font-family: 'lato', sans-serif;
+}
+
+/************************ START NAV STYLING ************************/
+.logo {
+  max-width: 200px;
+  display: block;
+  margin: auto;
+  position: absolute;
+  top: 8%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.nav-flex,
+.desktop-menu {
+  display: none;
+}
+.menu,
+.hamburger,
+.cross {
+  display: block;
+}
+/************************** Hamburger menu *********************/
+.hamburger {
+  background: none;
+  position: absolute;
+  top: 0;
+  line-height: 50px;
+  padding: 1px 10px 2px 15px;
+  color: #ffffff;
+  border: 0;
+  font-size: 1.8em;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  z-index: 10000000000000;
+}
+.cross {
+  background: none;
+  position: absolute;
+  top: 0;
+  padding: 1px 15px 0px 15px;
+  color: #ffffff;
+  border: 0;
+  font-size: 3.5em;
+  line-height: 65px;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  z-index: 10000000000000;
+}
+.menu {
+  z-index: 1000000;
+  font-weight: bold;
+  font-size: 0.8em;
+  width: 100%;
+  background: #D7745B;
+  position: absolute;
+  text-align: center;
+  font-size: 1em;
+}
+.menu ul {
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+  list-style-image: none;
+}
+.menu li {
+  display: block;
+  padding: 15px 0 15px 0;
+  border-bottom: #ffffff 1px solid;
+}
+.menu li:hover {
+  display: block;
+  background: #ffffff;
+  padding: 15px 0 15px 0;
+  border-bottom: white 1px solid;
+  transition: 0.2s ease;
+}
+.menu ul li a {
+  text-decoration: none;
+  margin: 0px;
+  color: #ffffff;
+}
+.menu ul li a:hover {
+  color: #D7745B;
+  text-decoration: none;
+}
+.menu a {
+  text-decoration: none;
+  color: #ffffff;
+}
+.menu a:hover {
+  text-decoration: none;
+  color: #ffffff;
+}
+/*********************** START FOOTER STYLING ***********************/
+.footer-color-block {
+  background-color: #05485A;
+  padding: 5%;
+  color: #ffffff;
+}
+.footer-color-block h3 {
+  text-align: center;
+  padding-bottom: 2%;
+}
+.footer-color-block ul,
+.footer-color-block p {
+  font-family: 'lato', sans-serif;
+  text-align: center;
+}
+.footer-color-block li {
+  list-style: none;
+}
+.footer-color-block p span {
+  font-weight: bold;
+}
+.footer-color-block .footer-padding {
+  padding-top: 8%;
+}
+.footer-content {
+  padding: 5%;
+}
+.footer-content ul,
+.footer-content p {
+  text-align: center;
+  color: #05485A;
+  font-family: 'lato', sans-serif;
+}
+.footer-content ul li,
+.footer-content p li {
+  list-style: none;
+  padding-bottom: 1%;
+}
+.footer-content .footer-icons {
+  text-align: center;
+  padding-top: 2%;
+  padding-bottom: 3%;
+  font-size: 35px;
+}
+.footer-content a {
+  color: #05485A;
+  text-decoration: none;
+  padding-right: 1%;
+}
+
+/*********************** START PAGE STYLING ***********************/
+
+main h2,
+main p {
+  color: #05485A;
+}
+
+</style>
+
+
+
+    <header>
+        <div class="logo-center">
+            <a href="index.html"><img class="logo" src="images/logo.png" title="3 Daughters Brewing Logo" alt="3 Daughters Brewing Logo" /></a>
+        </div>
+
+        <!--Desktop Menu-->
+        <nav class="desktop-menu">
+            <ul class="nav-flex">
+                <li><a href="about.html">ABOUT</a></li>
+                <li class="nav-beers"><a href="beers.html">BREWS</a></li>
+                <li><a href="events.html">EVENTS</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+            </ul>
+        </nav>
+
+        <!--Mobile Menu-->
+
+        <button class="hamburger">&#9776;</button>
+        <button class="cross">&#735;</button>
+
+        <nav class="menu">
+            <ul>
+                <li><a href="about.html">ABOUT</a></li>
+                <li class="nav-beers"><a href="beers.html">BREWS</a></li>
+                <li><a href="events.html">EVENTS</a></li>
+                <li><a href="contact.html">CONTACT</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+      <h2>Oops something went wrong!</h2>
+      <p>Please refresh the page and try again.</p>
+    </main>
+
+    <footer>
+
+<div class="footer-color-block">
+    <h3>HOURS</h3>
+    <ul>
+        <li>Monday - Thursday 11:30AM - 10:00PM</li>
+        <li>Friday - Saturday 11:30AM - 12:00AM</li>
+        <li>Sunday 11:30AM - 8:00PM</li>
+    </ul>
+
+    <div class="footer-padding">
+        <p><span>PHONE NUMBER:</span> 727-495-6002</p>
+        <p><span>ADDRESS:</span> 222 22nd Street South, St. Petersburg, FL 33712</p>
+    </div>
+</div>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14121.28775866159!2d-82.6627778!3d27.7690528!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4d9834cf3055fece!2s3%20Daughters%20Brewing!5e0!3m2!1sen!2sus!4v1623001621032!5m2!1sen!2sus" width="600"
+    height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+
+
+<div class="footer-content">
+    <ul>
+        <li><a href="about.html">ABOUT</a></li>
+        <li><a href="beers.html">BREWS</a></li>
+        <li><a href="events.html">EVENTS</a></li>
+        <li><a href="contact.html">CONTACT</a></li>
+    </ul>
+
+    <div class="footer-icons">
+        <a href="https://www.facebook.com/3DaughtersBrewing" target="_blank"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/3daughtersbrewing/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://twitter.com/3DBrewing" target="_blank"><i class="fab fa-twitter"></i></a>
+    </div>
+
+    <div>
+        <p>Designed by GG Creative Co.</p>
+        <p>All Rights Reserved by Three Daughters Brewing</p>
+        <p>Copyright 2021</p>
+    </div>
+
+</div>
+
+</footer>
+
+
+
+
+
